7d42379a58b0e22b6efe5ba6b5116b5c4ab600d2
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-10-08 18:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function removeSelectedColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex != -1) {
+        colorSelect.remove(selectedIndex);
+    } else {
+        alert('Please select a color to remove.');
+    }
+}
+document.getElementById('removeButton').addEventListener('click', removeSelectedColor);
