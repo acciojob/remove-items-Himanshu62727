4957b6f9a84caef9f90944ca1a6d25228474f766
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-09-14 21:35:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const allColor = document.querySelector('#colorSelect');
+const btn = document.querySelector('input');
+
+btn.addEventListener('click', () => {
+	allColor.remove([0]);
+});
